4e64f9657221992c2ce8803023b489fa3268cd6e
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-11 23:12:47 +0100

    Added registers to the CPU

--- src/gb/cpu.rs
+++ src/gb/cpu.rs
@@ -23,16 +23,106 @@
 */
 
 use gb::register_identifier::*;
+use gb::register::Register;
+use gb::register_8bit::Register8Bit;
+use gb::bi_register_8bit::BiRegister8Bit;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::RefCell;
 
+///
+/// Represents the GameBoy CPU
+///
 pub struct CPU {
-
+    ///
+    /// Contains the 8 registers of the GameBoy CPU (A, B, C, D, E, F, H, L)
+    ///
+    registers: HashMap<RegisterIdentifier, Rc<RefCell<Register8Bit>>>,
+    ///
+    /// Contains the 4 "bi-registers" of the GameBoy CPU
+    /// A "bi-register" is two registers used as a bigger one
+    /// The GameBoy CPU has 4 bi-registers: AF, BC, DE, HL
+    ///
+    bi_registers: HashMap<BiRegisterIdentifier, BiRegister8Bit>
 }
 
 impl CPU {
     pub fn new() -> CPU {
-        CPU {
 
+        let mut registers = HashMap::new();
+        registers.insert(RegisterIdentifier::A, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::B, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::C, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::D, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::E, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::F, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::H, Rc::new(RefCell::new(Register8Bit::new())));
+        registers.insert(RegisterIdentifier::L, Rc::new(RefCell::new(Register8Bit::new())));
+
+        let mut bi_registers = HashMap::new();
+        bi_registers.insert(BiRegisterIdentifier::AF,
+                            BiRegister8Bit::new(Rc::clone(&registers[&RegisterIdentifier::A]),
+                                                Rc::clone(&registers[&RegisterIdentifier::F])));
+        bi_registers.insert(BiRegisterIdentifier::BC,
+                            BiRegister8Bit::new(Rc::clone(&registers[&RegisterIdentifier::B]),
+                                                Rc::clone(&registers[&RegisterIdentifier::C])));
+        bi_registers.insert(BiRegisterIdentifier::DE,
+                            BiRegister8Bit::new(Rc::clone(&registers[&RegisterIdentifier::D]),
+                                                Rc::clone(&registers[&RegisterIdentifier::E])));
+        bi_registers.insert(BiRegisterIdentifier::HL,
+                            BiRegister8Bit::new(Rc::clone(&registers[&RegisterIdentifier::H]),
+                                                Rc::clone(&registers[&RegisterIdentifier::L])));
+
+        CPU {
+            registers,
+            bi_registers
         }
+    }
+
+    ///
+    /// Writes a value into a register
+    ///
+    /// Params:
+    /// - register_identifier: RegisterIdentifier = The identifier of the register to write to
+    /// - value: u8 = The value to write into the register
+    ///
+    fn write_register(&mut self, register_identifier: RegisterIdentifier, value: u8) {
+        self.registers.get_mut(&register_identifier).unwrap().borrow_mut().write(value);
+    }
+    ///
+    /// Reads a value from a register
+    ///
+    /// Params:
+    /// - register_identifier: RegisterIdentifier = The identifier of the register to read from
+    ///
+    /// Return:
+    /// - The register value
+    ///
+    fn read_register(&self, register_identifier: RegisterIdentifier) -> u8 {
+        self.registers.get(&register_identifier).unwrap().borrow().read()
+    }
+
+    ///
+    /// Writes a value into a bi-register
+    ///
+    /// Params:
+    /// - register_identifier: RegisterIdentifier = The identifier of the bi-register to write to
+    /// - value: u16 = The value to write into the register
+    ///
+    fn write_bi_register(&mut self, register_identifier: BiRegisterIdentifier, value: u16) {
+        self.bi_registers.get_mut(&register_identifier).unwrap().write(value);
+    }
+    ///
+    /// Reads a value from a bi-register
+    ///
+    /// Params:
+    /// - register_identifier: RegisterIdentifier = The identifier of the bi-register to read from
+    ///
+    /// Return:
+    /// - The bi-register value
+    ///
+    fn read_bi_register(&self, register_identifier: BiRegisterIdentifier) -> u16 {
+        self.bi_registers.get(&register_identifier).unwrap().read()
     }
 }
 
@@ -43,18 +133,34 @@ mod test {
 
     #[test]
     fn can_write_to_register() {
-        let cpu = CPU::new();
+        let mut cpu = CPU::new();
         cpu.write_register(RegisterIdentifier::A, 0x15);
 
-        assert_eq!(cpu.registers[RegisterIdentifier::A], 0x15);
+        assert_eq!(cpu.registers.get(&RegisterIdentifier::A).unwrap().borrow().read(), 0x15);
     }
 
     #[test]
     fn can_read_from_register() {
         let mut cpu = CPU::new();
-        cpu.registers[RegisterIdentifier::A] = 0x15;
+        cpu.registers.get_mut(&RegisterIdentifier::A).unwrap().borrow_mut().write(0x05);
 
-        assert_eq!(cpu.read_register(RegisterIdentifier::A), 0x15);
+        assert_eq!(cpu.read_register(RegisterIdentifier::A), 0x05);
+    }
+
+    #[test]
+    fn can_write_to_bi_register() {
+        let mut cpu = CPU::new();
+        cpu.write_bi_register(BiRegisterIdentifier::AF, 0x1234);
+
+        assert_eq!(cpu.bi_registers.get(&BiRegisterIdentifier::AF).unwrap().read(), 0x1234);
+    }
+
+    #[test]
+    fn can_read_from_bi_register() {
+        let mut cpu = CPU::new();
+        cpu.bi_registers.get_mut(&BiRegisterIdentifier::AF).unwrap().write(0x1234);
+
+        assert_eq!(cpu.read_bi_register(BiRegisterIdentifier::AF), 0x1234);
     }
 }
 
