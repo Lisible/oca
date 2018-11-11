c4609173133f326cc2cad4bf896d93007567f4b6
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-11 23:11:22 +0100

    Created a Register trait

---
+++ src/gb/register.rs
@@ -0,0 +1,47 @@
+/*
+* MIT License
+*
+* Copyright (c) 2018 Clément SIBILLE
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+///
+/// Trait defining a register
+///
+/// Params:
+/// - T = The type of the value of the register
+///
+pub trait Register<T> {
+    ///
+    /// Writes a value into the register
+    ///
+    /// Params:
+    /// - value: T = The value to write into the register
+    ///
+    fn write(&mut self, value: T);
+    ///
+    /// Reads the value from the register
+    ///
+    ///
+    /// Returns:
+    /// - The value of the register
+    ///
+    fn read(&self) -> T;
+}
